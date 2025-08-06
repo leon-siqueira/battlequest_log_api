1d0eb053099bd44cab242f1512637c98dcd049bf
Leon Siqueira <leon.siqueir4@gmail.com>
2025-08-05 21:43:41 -0300

    feat: add score column to players table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_06_000922) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_06_004211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_06_000922) do
     t.integer "hp", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score", default: 0, null: false
     t.index ["logged_id"], name: "index_players_on_logged_id", unique: true
   end
 end
