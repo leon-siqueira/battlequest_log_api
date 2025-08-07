ff824897ed608479ca92c5ec1bccb8c878a84e28
Leon Siqueira <leon.siqueir4@gmail.com>
2025-08-07 20:19:20 -0300

    refactor: remove memoization since it may impact on the finding of players yagni: remove player_quest_spec

--- app/models/concerns/event_handler/player_join.rb
+++ app/models/concerns/event_handler/player_join.rb
@@ -2,8 +2,8 @@ class EventHandler::PlayerJoin < EventHandler::Base
   private
 
   def run
-    @player ||= Player.find_or_initialize_by(logged_id: @data["id"])
-    @player.assign_attributes(name: @data["name"], level: @data["level"])
-    @player.save!
+    player = Player.find_or_initialize_by(logged_id: @data["id"])
+    player.assign_attributes(name: @data["name"], level: @data["level"])
+    player.save!
   end
 end
