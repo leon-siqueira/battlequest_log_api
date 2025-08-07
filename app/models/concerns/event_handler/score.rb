d5ae1cfc87b1b13111ed1bfacbec0811c4bea4bf
Leon Siqueira <leon.siqueir4@gmail.com>
2025-08-07 19:38:16 -0300

    feat: score event

---
+++ app/models/concerns/event_handler/score.rb
@@ -0,0 +1,9 @@
+class EventHandler::Score < EventHandler::Base
+  private
+
+  def run
+    @player = Player.find_or_initialize_by(logged_id: @data["player_id"])
+    @player.score += @data["score"].to_i
+    @player.save!
+  end
+end
