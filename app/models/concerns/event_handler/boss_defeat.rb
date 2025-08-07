bc3286919bf8c9f05c1987593f48745a6906a2d1
Leon Siqueira <leon.siqueir4@gmail.com>
2025-08-07 19:34:30 -0300

    feat: boss_defeat event

---
+++ app/models/concerns/event_handler/boss_defeat.rb
@@ -0,0 +1,8 @@
+class EventHandler::BossDefeat < EventHandler::Base
+  def run
+    @player = Player.find_or_initialize_by(logged_id: @data["defeated_by"])
+    @player.xp += @data["xp"].to_i
+    @player.gold += @data["gold"].to_i
+    @player.save!
+  end
+end
