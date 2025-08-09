3f68adc4f0e536878df6341344486537438a05b9
Leon Siqueira <leon.siqueir4@gmail.com>
2025-08-08 22:27:55 -0300

    feat: no record error handling response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found(e)
+    render json: { error: e.message }, status: :not_found
+  end
 end
